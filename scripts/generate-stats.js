4ad6d3d4eafa6afdf701dc6a1e054d2338ec825c
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-27 13:19:54 -0300

    docs: script para gerar quantidade de projetos em desenvolvimento e em produção com fetch na API do Github

---
+++ scripts/generate-stats.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+(async () => {
+    const token = process.env.GH_TOKEN;
+
+    const response = await fetch(`https://api.github.com/users/Octal-web/repos`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/vnd.github+json',
+        },
+    });
+
+    const repos = await response.json();
+
+    let development = 0;
+    let production = 0;
+
+    repos.forEach((repo) => {
+        const topics = repo.topics || [];
+
+        if (topics.includes('development')) {
+            development++;
+        }
+
+        if (topics.includes('production')) {
+            production++;
+        }
+    });
+
+    const newContent = `
+    + Projetos em produção: ${production}
+
+    + Projetos em desenvolvimento: ${development}
+    `;
+
+    let readme = fs.readFileSync('README.md', 'utf8');
+
+    readme = readme.replace(/<!--PROJECT_STATS_START-->[\s\S]*<!--PROJECT_STATS_END-->/, newContent);
+
+    fs.writeFileSync('README.md', readme);
+})();
